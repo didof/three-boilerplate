d0466f20a0ca51f5e7be0dbf898568871127567c
didof <didonato.fr@gmail.com>
2021-05-03 09:06:42 +0200

    refactor: created InitCamera and InitControls

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
 import gui, {
   debugObj,
   debugToneMappingType,
@@ -21,11 +22,20 @@ export default class App {
       height: canvas.height || window.innerHeight,
     }
 
+    this.previousTime = 0
+
     /**
      * Loaders
      */
     this.textureLoader = new THREE.TextureLoader()
     this.cubeTextureLoader = new THREE.CubeTextureLoader()
+
+    this.playerMixer = {
+      update() {},
+    }
+
+    this.fbxModelLoader = new FBXLoader()
+    this.fbxAnimationsLoader = new FBXLoader()
 
     this.Initialize(canvas.querySelector)
 
@@ -47,7 +57,7 @@ export default class App {
       skyboxFolderName: 'openFields',
     })
 
-    buildDirectionalLight(this.scene, true)
+    buildDirectionalLight(this.scene)
 
     const test = new THREE.Mesh(
       new THREE.SphereBufferGeometry(1, 32, 32),
@@ -61,7 +71,7 @@ export default class App {
     test.receiveShadow = true
     gui.add(test.material, 'metalness', 0, 1, 0.01)
     gui.add(test.material, 'roughness', 0, 1, 0.01)
-    this.scene.add(test)
+    // this.scene.add(test)
 
     const floor = new THREE.Mesh(
       new THREE.PlaneBufferGeometry(10, 10),
@@ -72,23 +82,28 @@ export default class App {
     floor.receiveShadow = true
     this.scene.add(floor)
 
-    /**
-     * Camera
-     */
-    this.camera = new THREE.PerspectiveCamera(
-      75,
-      this.sizes.width / this.sizes.height,
-      0.1,
-      100
-    )
-    this.camera.position.set(0.25, -0.25, 4)
-    this.scene.add(this.camera)
+    this.InitCamera()
 
-    this.controls = new OrbitControls(this.camera, canvas)
-    this.controls.target.set(0, 0, 0)
-    this.controls.enableDamping = true
+    this.InitControls({ canvas })
 
     this.updateAllMaterials()
+
+    /**
+     * Knight
+     */
+    this.fbxModelLoader.load('/models/fbx/knight.fbx', fbx => {
+      fbx.scale.setScalar(0.01)
+      fbx.traverse(child => {
+        child.castShadow = true
+      })
+
+      this.fbxAnimationsLoader.load('/models/fbx/Idle.fbx', anim => {
+        this.mixer = new THREE.AnimationMixer(fbx)
+        this.walk = this.mixer.clipAction(fbx.animations[1])
+        this.walk.play()
+      })
+      this.scene.add(fbx)
+    })
   }
 
   InitScene = ({ skyboxFolderName }) => {
@@ -108,6 +123,22 @@ export default class App {
     this.scene.environment = environmentMap
   }
 
+  InitCamera = () => {
+    const fov = 75
+    const aspect = this.sizes.width / this.sizes.height
+    const near = 0.1
+    const far = 100
+    this.camera = new THREE.PerspectiveCamera(fov, aspect, near, far)
+    this.camera.position.set(0.25, -0.25, 4)
+    this.scene.add(this.camera)
+  }
+
+  InitControls = ({ canvas }) => {
+    this.controls = new OrbitControls(this.camera, canvas)
+    this.controls.target.set(0, 0, 0)
+    this.controls.enableDamping = true
+  }
+
   InitRenderer = ({ canvas }) => {
     this.renderer = new THREE.WebGLRenderer({
       canvas,
@@ -117,8 +148,8 @@ export default class App {
     this.renderer.outputEncoding = THREE.sRGBEncoding
 
     this.renderer.toneMapping = THREE.ACESFilmicToneMapping
-    this.renderer.toneMappingExposure = 2
     debugToneMappingType(this.renderer, this.updateAllMaterials)
+    this.renderer.toneMappingExposure = 2
     debugToneMappingExposure(this.renderer)
 
     this.renderer.shadowMap.enabled = true
@@ -160,7 +191,11 @@ export default class App {
   }
 
   Tick = () => {
-    const et = this.clock.getElapsedTime()
+    const elapsedTime = this.clock.getElapsedTime()
+    const deltaTime = elapsedTime - this.previousTime
+    this.previousTime = elapsedTime
+
+    this.playerMixer.update(deltaTime)
 
     // Update controls
     this.controls.update()
